e829247d8434c0e137bf1c39de2c38396410909e
Razdor13 <oneamaday@gmail.com>
2023-12-11 22:57:08 +0200

    done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const numbers = [
 let positiveNumbers = [];
 let negativeNumbers = [];
 let positiveNumbersEven = [];
+let positiveNumbersNotEven = [];
 let positiveNumbersSum = 0;
 let productNumbers = 1;
 let bigNumber = 0;
@@ -22,35 +23,15 @@ for (let i = 0; i < numbers.length; i++) {
         positiveNumbers.push(numbers[i]);
         positiveNumbersSum += numbers[i];
         productNumbers *= numbers[i];
+        if (numbers[i] % 2 !== 0) {
+            positiveNumbersNotEven.push(numbers[i]);
+        }
+        if (numbers[i] % 2 === 0) {
+            positiveNumbersEven.push(numbers[i]);
+        }
     }
     if (numbers[i] < 0) {
         negativeNumbers.push(numbers[i]);
-    }
-}
-
-let positiveNumbersNotEven = positiveNumbers.slice(0);
-
-for (let m = 0; m < positiveNumbersNotEven.length; m++) {
-    for (let j = m + 1; j < positiveNumbersNotEven.length; j++) {
-        if (positiveNumbersNotEven[m] === positiveNumbersNotEven[j]) {
-            positiveNumbersEven.push(
-                positiveNumbersNotEven[m],
-                positiveNumbersNotEven[j]
-            );
-            positiveNumbersNotEven[m] = null;
-            positiveNumbersNotEven[j] = null;
-        }
-    }
-}
-
-for (let iter = 0; iter < positiveNumbersNotEven.length; iter++) {
-    if (positiveNumbersNotEven[iter] === null) {
-        positiveNumbersNotEven.splice(iter, 1);
-        iter--;
-    }
-    if (positiveNumbersEven[iter] === null) {
-        positiveNumbersEven.splice(iter, 1);
-        iter--;
     }
 }
 
@@ -69,3 +50,5 @@ alert(`Сума позитивних елементів ${positiveNumbersSum}
 Сума парних позитивних елементів  ${positiveNumbersEvenSum}
 Сума непарних позитивних елементів  ${positiveNumbersNotEvenSum}
 Добуток позитивних елементів ${productNumbers}`);
+
+console.log(positiveNumbersEven, positiveNumbersNotEven);
